c015d02b47dfcbd68d942e643820030fceaca698
theGVK <116721578+theGVK@users.noreply.github.com>
2023-07-04 22:08:13 +0530

    maxprofit

---
+++ maxprofit.cpp
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minPrice, maxProfit  = float('inf'), 0
+        for price in prices:
+            if price < minPrice:
+                minPrice = price
+            elif price - minPrice > maxProfit:
+                maxProfit = price - minPrice
+        return maxProfit
